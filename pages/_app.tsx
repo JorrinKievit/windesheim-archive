26d818b72c60fac8dc0ac40d30bfdb01b75b0dbd
Jorrin <jorrinkievit@hotmail.com>
2022-06-28 22:58:53 +0200

    readme + app title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,53 +2,63 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Box, Text, MantineProvider, Title } from "@mantine/core";
 import TextLink from "../components/TextLink";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      theme={{ colorScheme: "dark" }}
-      withNormalizeCSS
-      withGlobalStyles
-    >
-      <Box
-        sx={(theme) => ({
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          borderBottom: `1px solid ${theme.colors.gray[8]}`,
-          height: "50px",
-          "@media (max-width: 340px)": {
-            height: "100px",
-          },
-        })}
+    <>
+      <Head>
+        <title>Windesheim Archive</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        theme={{ colorScheme: "dark" }}
+        withNormalizeCSS
+        withGlobalStyles
       >
-        <Title>Windesheim Archive</Title>
-      </Box>
-      <Component {...pageProps} />
-      <Box
-        sx={(theme) => ({
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          borderTop: `1px solid ${theme.colors.gray[8]}`,
-          height: "50px",
-          "@media (max-width: 340px)": {
-            height: "100px",
-          },
-        })}
-      >
-        <Text>
-          Made by{" "}
-          <TextLink
-            href="https://www.linkedin.com/in/JorrinKievit/"
-            text="Jorrin Kievit"
-          />
-          , Credits to Steph#3907 on Discord
-        </Text>
-      </Box>
-    </MantineProvider>
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            borderBottom: `1px solid ${theme.colors.gray[8]}`,
+            height: "50px",
+            "@media (max-width: 340px)": {
+              height: "100px",
+            },
+          })}
+        >
+          <Title>Windesheim Archive</Title>
+        </Box>
+        <Component {...pageProps} />
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            borderTop: `1px solid ${theme.colors.gray[8]}`,
+            height: "50px",
+            "@media (max-width: 340px)": {
+              height: "100px",
+            },
+          })}
+        >
+          <Text>
+            Made by{" "}
+            <TextLink
+              href="https://www.linkedin.com/in/JorrinKievit/"
+              text="Jorrin Kievit"
+            />
+            , Credits to Steph#3907 on Discord
+          </Text>
+        </Box>
+      </MantineProvider>
+    </>
   );
 }
 
